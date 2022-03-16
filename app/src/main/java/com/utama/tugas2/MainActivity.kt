8a9230320fd01a793042c72ba41f6716b0f9c2b9
sintaauliya <sinta.auliya@widyatama.ac.id>
2022-03-16 17:01:28 +0700

    Initial commit

---
+++ app/src/main/java/com/utama/tugas2/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.utama.tugas2
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    val id: Int = 10
+    val language: String = "kotlin"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)
+        button.setOnClickListener() {
+            intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("id_value", id)
+            intent.putExtra("language_value", language)
+            startActivity(intent)
+        }
+    }
+}
